7f3096d253d6b5bfa5df9d2f178c29d0c6d34d13
Wouter Lindeboom <Wouterlindeboom@hotmail.lv>
2016-01-18 11:43:05 +0100

    Image background test

--- Project Game/Project_Game.py
+++ Project Game/Project_Game.py
@@ -1,2 +1,18 @@
-print('test')
-print ('moretest')
+import sys
+import pygame
+
+pygame.init()
+
+screensize = width, height = 500, 500
+board = pygame.image.load('Images/test.png')
+
+screen = pygame.display.set_mode(screensize)
+#screen.fill(black)
+
+def main():
+    while True:
+        screen.fill([255,255,255])
+        screen.blit(board, (0,0))
+        pygame.display.flip()
+
+main()
