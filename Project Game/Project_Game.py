0bfa7a1714765159f7538576652f5f7bf8e3e7e0
Wouter Lindeboom <Wouterlindeboom@hotmail.lv>
2016-01-18 14:55:45 +0100

    Fixed inverted colour

--- Project Game/Project_Game.py
+++ Project Game/Project_Game.py
@@ -5,15 +5,15 @@ import Tile
 pygame.init()
 
 screensize = width, height = 1000, 600
-board = pygame.image.load('Images/board.jpg')
-resizedboard = pygame.transform.scale(board, (600,600))
+circle = pygame.transform.scale(pygame.image.load('Images/circle.jpg'), (30,30))
+board = pygame.transform.scale(pygame.image.load('Images/board.png'), (600,600))
 
 screen = pygame.display.set_mode(screensize)
 #screen.fill(black)
 
 def main():
     while True:#Main game loop
-       screen.fill((0,0,0))
-       screen.blit(resizedboard, (0, 0))
-       pygame.display.flip()
+       screen.blit(board, (0, 0))
+       screen.blit(circle, (10,10))
+       pygame.display.update()
 main()
