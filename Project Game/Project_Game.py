b99895ac64b985291ef53a656a455f0405cd9af7
Wouter Lindeboom <Wouterlindeboom@hotmail.lv>
2016-01-18 14:17:56 +0100

    Small start with loading the image. Also added Tile.py for tile support.

--- Project Game/Project_Game.py
+++ Project Game/Project_Game.py
@@ -1,18 +1,19 @@
 import sys
 import pygame
+import Tile
 
 pygame.init()
 
-screensize = width, height = 500, 500
-board = pygame.image.load('Images/test.png')
+screensize = width, height = 1000, 600
+board = pygame.image.load('Images/board.jpg')
+resizedboard = pygame.transform.scale(board, (600,600))
 
 screen = pygame.display.set_mode(screensize)
 #screen.fill(black)
 
 def main():
-    while True:
-        screen.fill([255,255,255])
-        screen.blit(board, (0,0))
-        pygame.display.flip()
-
+    while True:#Main game loop
+       screen.fill((0,0,0))
+       screen.blit(resizedboard, (0, 0))
+       pygame.display.flip()
 main()
